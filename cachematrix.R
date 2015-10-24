29082218b2e8f9836ba249fff558c732b32496b4
jakubnowicki <j.nowicki@student.uw.edu.pl>
2015-10-24 15:54:43 +0200

    working functons without comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    set.inverted <- function(inverted) inv <<- inverted
+    get.inverted <- function() inv
+    list(set = set, get = get,
+         set.inverted = set.inverted,
+         get.inverted = get.inverted)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$get.inverted()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$set.inverted(inv)
+    inv
 }
